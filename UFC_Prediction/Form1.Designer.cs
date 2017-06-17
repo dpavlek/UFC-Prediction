77e7f7c368b8852d83c0dd9878d21e9969870335
Daniel Pavleković <daniel.pavlekovic@gmail.com>
2017-06-17 19:17:33 +0200

    Added pictureboxes and a button

--- UFC_Prediction/Form1.Designer.cs
+++ UFC_Prediction/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.blueFighterPic = new System.Windows.Forms.PictureBox();
+            this.redFighterPic = new System.Windows.Forms.PictureBox();
+            this.startFightBtn = new System.Windows.Forms.Button();
+            this.blueFighterCbx = new System.Windows.Forms.ComboBox();
+            this.redFighterCbx = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.blueFighterPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redFighterPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // blueFighterPic
+            // 
+            this.blueFighterPic.BackColor = System.Drawing.Color.White;
+            this.blueFighterPic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.blueFighterPic.Location = new System.Drawing.Point(12, 12);
+            this.blueFighterPic.Name = "blueFighterPic";
+            this.blueFighterPic.Size = new System.Drawing.Size(264, 347);
+            this.blueFighterPic.TabIndex = 0;
+            this.blueFighterPic.TabStop = false;
+            // 
+            // redFighterPic
+            // 
+            this.redFighterPic.BackColor = System.Drawing.Color.White;
+            this.redFighterPic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.redFighterPic.Location = new System.Drawing.Point(776, 12);
+            this.redFighterPic.Name = "redFighterPic";
+            this.redFighterPic.Size = new System.Drawing.Size(264, 347);
+            this.redFighterPic.TabIndex = 1;
+            this.redFighterPic.TabStop = false;
+            // 
+            // startFightBtn
+            // 
+            this.startFightBtn.Location = new System.Drawing.Point(448, 467);
+            this.startFightBtn.Name = "startFightBtn";
+            this.startFightBtn.Size = new System.Drawing.Size(75, 23);
+            this.startFightBtn.TabIndex = 2;
+            this.startFightBtn.Text = "Fight!";
+            this.startFightBtn.UseVisualStyleBackColor = true;
+            this.startFightBtn.Click += new System.EventHandler(this.startFightBtn_Click);
+            // 
+            // blueFighterCbx
+            // 
+            this.blueFighterCbx.FormattingEnabled = true;
+            this.blueFighterCbx.Location = new System.Drawing.Point(12, 365);
+            this.blueFighterCbx.Name = "blueFighterCbx";
+            this.blueFighterCbx.Size = new System.Drawing.Size(264, 21);
+            this.blueFighterCbx.TabIndex = 3;
+            // 
+            // redFighterCbx
+            // 
+            this.redFighterCbx.FormattingEnabled = true;
+            this.redFighterCbx.Location = new System.Drawing.Point(776, 366);
+            this.redFighterCbx.Name = "redFighterCbx";
+            this.redFighterCbx.Size = new System.Drawing.Size(264, 21);
+            this.redFighterCbx.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 701);
+            this.Controls.Add(this.redFighterCbx);
+            this.Controls.Add(this.blueFighterCbx);
+            this.Controls.Add(this.startFightBtn);
+            this.Controls.Add(this.redFighterPic);
+            this.Controls.Add(this.blueFighterPic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.blueFighterPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redFighterPic)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox blueFighterPic;
+        private System.Windows.Forms.PictureBox redFighterPic;
+        private System.Windows.Forms.Button startFightBtn;
+        private System.Windows.Forms.ComboBox blueFighterCbx;
+        private System.Windows.Forms.ComboBox redFighterCbx;
     }
 }
 
--- UFC_Prediction/Form1.cs
+++ UFC_Prediction/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CallRequestResponseService;
+
 
 namespace UFC_Prediction
 {
+
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startFightBtn_Click(object sender, EventArgs e)
+        {
+            Azure.InvokeRequestResponseService().Wait();
         }
     }
 }
